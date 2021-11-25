da721c4cc95f678957220549a2343d4f3981e7e0
Esraa_Kamel <esraa.kamel1811@gmail.com>
2021-11-25 15:17:26 +0200

    Set demo and connent to database

--- index.js
+++ index.js
@@ -8,9 +8,48 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
-app.use(bodyParser.json);
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+const DBUrl = "mongodb://localhost:27017/ChatApp";
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
+
+mongoose.connect(DBUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err)=> {
+    console.log('MongoDb connection', err);
+});
+
+app.get('/messages', (req, res) => {
+    Message.find({}, (err, messages) =>{
+        res.send(messages)
+    });
+});
+
+app.post('/messages', (req, res) => {
+
+    let message = new Message(req.body);
+
+    message.save((err) => {
+        if (err) {
+            res.sendStatus = 500;
+            console.error(err);
+        }
+        io.emit('message', req.body);
+        res.sendStatus(200);
+    });
+});
+
+io.on('connection', (socket)=> {
+    console.log(`user connected`);
+});
+
 
 const server = http.listen('3000', () => {
-    console.log(`Server is lestening on port ${ server.address().port }`);
+    console.log(`Server is listening on port ${ server.address().port }`);
 });
