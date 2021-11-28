d6416229e3d09d2e405640dea034d5d9d738f10a
Esraa_Kamel <esraa.kamel1811@gmail.com>
2021-11-28 23:48:43 +0200

    Trying test with Jasmine

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -1,5 +1,39 @@
+const request = require('request');
+
 describe('calc', () => {
     it(`should multiply 2 and 2`, () => {
         expect(2*2).toBe(4)
+    });
+});
+
+describe('get messages', () => {
+    it(`Should return 200 ok`, (done) => {
+        request.get('http://localhost:3000/messages', ( err, res ) => {
+            expect(res.statusCode).toEqual(200);
+            done();
+        });
+    });
+
+    it(`Should return a list, that is not empty`, (done) => {
+        request.get('http://localhost:3000/messages', ( err, res ) => {
+            expect(JSON.parse(res.body).length).toBeGreaterThan(0);
+            done();
+        });
+    });
+});
+
+describe('get messages from user', () => {
+    it(`Should return 200 ok`, (done) => {
+        request.get('http://localhost:3000/messages/esraa', ( err, res ) => {
+            expect(res.statusCode).toEqual(200);
+            done();
+        });
+    });
+
+    it(`Name should be esraa`, (done)=> {
+        request.get('http://localhost:3000/messages/esraa', ( err, res ) => {
+            expect(JSON.parse(res.body)[0].name).toEqual('esraa');
+            done();
+        });
     });
 });
